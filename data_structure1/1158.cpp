a6ee69110de4398c159c425eff36c7f61b7a9958
yw1222-park <dpdnjs1222@naver.com>
2021-04-05 23:56:04 +0900

    queue: update 1158 fancy solution

--- data_structure1/1158.cpp
+++ data_structure1/1158.cpp
@@ -4,17 +4,7 @@
 
 using namespace std;
 
-int main(){
-
-    queue<int> q;
-
-    int n, k;
-    cin >> n >> k;
-
-    for(int i=1; i<=n; i++)
-        q.push(i);
-
-    cout << '<';
+void my_solution(queue<int> q, int n, int k){
     int i = 1;
     while(!q.empty()){
         int now = q.front();
@@ -28,5 +18,31 @@ int main(){
         }
         i++;
     }
+}
+
+void another_solution(queue<int> q, int n, int k){
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<k-1; j++){
+            q.push(q.front());
+            q.pop();
+        }
+        cout << q.front() << ", ";
+        q.pop();
+    }
+    cout << q.front();
+}
+
+int main(){
+
+    queue<int> q;
+
+    int n, k;
+    cin >> n >> k;
+
+    for(int i=1; i<=n; i++)
+        q.push(i);
+
+    cout << '<';
+    my_solution(q, n, k);
     cout << '>';
 }
